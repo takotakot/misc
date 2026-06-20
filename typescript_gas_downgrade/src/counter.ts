fc3b7c47d3b075c9d6452c38f8c90b8a786e8d1f
Takuho NAKANO <takotakot@users.noreply.github.com>
2026-06-20 16:17:17 +0900

    style: コードのフォーマットを統一し、スペースを削除

--- typescript_gas_downgrade/src/counter.ts
+++ typescript_gas_downgrade/src/counter.ts
@@ -1,10 +1,10 @@
-import { createMachine, assign } from 'xstate';
+import {createMachine, assign} from 'xstate';
 
 type CounterContext = {
   count: number;
 };
 
-export type CounterEvent = { type: 'INCREMENT' } | { type: 'RESET' };
+export type CounterEvent = {type: 'INCREMENT'} | {type: 'RESET'};
 
 export const counterMachine = createMachine({
   types: {} as {
@@ -13,15 +13,15 @@ export const counterMachine = createMachine({
   },
   id: 'counterMachine',
   initial: 'active',
-  context: { count: 0 },
+  context: {count: 0},
   states: {
     active: {
       on: {
         INCREMENT: {
-          actions: assign({ count: ({ context }) => context.count + 1 }),
+          actions: assign({count: ({context}) => context.count + 1}),
         },
         RESET: {
-          actions: assign({ count: 0 }),
+          actions: assign({count: 0}),
         },
       },
     },
