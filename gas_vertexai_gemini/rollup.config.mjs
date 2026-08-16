69ab7051a591461f70d28b727ecf8b6a600ad340
takotakot <takotakot@users.noreply.github.com>
2026-08-16 11:30:01 +0900

    feat: Add Rollup configuration for TypeScript build process

---
+++ gas_vertexai_gemini/rollup.config.mjs
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import cleanup from 'rollup-plugin-cleanup';
+import prettier from 'rollup-plugin-prettier';
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    dir: 'dist',
+    format: 'esm',
+  },
+  plugins: [
+    cleanup({comments: 'none', extensions: ['.ts']}),
+    typescript(),
+    prettier({parser: 'typescript'}),
+  ],
+  context: 'this',
+};
